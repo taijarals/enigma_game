90ce6aff5ca77ccda2fdbc89478cdcba49d7fc37
taijarals <taijara@outlook.com.br>
2026-04-08 21:00:20 -0300

    secrets

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from supabase import create_client, Client
 # =========================
 # CONFIG SUPABASE
 # =========================
-SUPABASE_URL = "https://YOUR_PROJECT.supabase.co"
-SUPABASE_KEY = "YOUR_ANON_KEY"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
